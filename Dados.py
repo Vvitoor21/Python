e09c2336713090e66ea7fd30549a0b48e3ecf7aa
Vitor <64867044+Vvitoor21@users.noreply.github.com>
2020-06-23 19:46:12 -0300

    Create Dados.py

---
+++ Dados.py
@@ -0,0 +1,37 @@
+print('Iteração de dados em Python')#Introdução
+n2 = 2#varival 1
+n3 = 3#variavel 2
+n5 = 5#varavel 3
+print('Usamos esse metodo para uma hipótese real: ||')#|| alguem é verdeiro?
+print('Usamos este método para comparação: &&')# && os dois precisam ser verdadeiros
+print(n5/n3)# imprimindo
+
+print(int(n5/n3))# impriminto inteiro
+print(list([n2+n3,n5*n2]))# fazendo uma lista com somas entre as variaveis
+nome='Vitor Duarte'# meu nome String
+print(nome)# imprimindo nome
+print(nome.count('D'))# contando quantas vezes a letra aparece
+print(nome.lower())# deixando tudo em minusculo
+print(nome.upper())# deixando tudo maiusculo
+print(nome.split())#separando os dois nomes
+print(list(nome))#dividindo todos os índices
+nome = list(nome)#tranformando em list
+print(nome)#
+print(type(nome))#
+nome.extend(' Bem')#colocando dento da lista
+print(nome)#
+nome=str(nome)#tranformando novamente a lista em String
+print(type(nome))#
+print('Alguns métodos servem somente para String e outros para Listas.')#
+print('Cada função tem seus métodos específicos.')#
+print('OPERAÇÕES ARITMETICOS:')#
+nu1=int(input('Digite um numero inteiro '))#
+nu2=int(input('Digite outro numero inteiro '))#
+print(type(nu1),type(nu2))#
+print('Operações essenciais(+,-,/,*)com os numeros digitados: ')#
+print(f'A soma de {nu1} e {nu2} é: {nu1+nu2}')#
+print(f'A diferença de {nu1} e {nu2} é: {nu1-nu2}')#
+print(f'A divisão de {nu1} e {nu2} é: {nu1//nu2}')#
+print(f'A multiplicação de {nu1} e {nu2} é: {nu1*nu2}')#
+a = True
+b = False
