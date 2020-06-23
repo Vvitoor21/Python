964c239715d78d98300f1bdd8caa4164a3f990cd
Vitor <64867044+Vvitoor21@users.noreply.github.com>
2020-06-23 19:44:14 -0300

    Create Calculadora.py

---
+++ Calculadora.py
@@ -0,0 +1,19 @@
+
+num=int(input('Digite um número para saber o resultado na calculadora: '))
+
+print(f'TABUADA DO NÚMERO {num}.')
+
+print(f'{num} * 1  é: {num * 1}')
+print(f'{num} * 2  é: {num * 2}')
+print(f'{num} * 3  é: {num * 3}')
+print(f'{num} * 4  é: {num * 4}')
+print(f'{num} * 5  é: {num * 5}')
+print(f'{num} * 6  é: {num * 6}')
+print(f'{num} * 7  é: {num * 7}')
+print(f'{num} * 8  é: {num * 8}')
+print(f'{num} * 9  é: {num * 9}')
+print(f'{num} * 10 é: {num *10}')
+
+num2=int(input('Digite um número para saber seu valor * 10: '))
+
+print(f'{num2}* 10 é {num2* 10}')
