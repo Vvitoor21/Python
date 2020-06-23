18f835e67475af61958f0d4b7d9a92d24ada4393
Vitor <64867044+Vvitoor21@users.noreply.github.com>
2020-06-23 19:46:40 -0300

    Create Dicionarios.py

---
+++ Dicionarios.py
@@ -0,0 +1,15 @@
+months={'Jan':'January','Feb':'February','Mar':'March','Apr':'April',}
+print('O dicionario completo é:')
+print(months)
+print('Os valores do dicionario são:')
+print(months.values())
+print('As chaves dos dicionarios são:')
+print(months.keys())
+months['Ma']='May'
+months['Ju']='June'
+months['Jul']='July'
+print(months)
+months['JAN'] =months['Jan']
+del months['Jan']
+print(months)
+print(months.items())
