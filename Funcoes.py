e5ce0ea1326b7583404450240dd7e71d9d651b71
Vitor <64867044+Vvitoor21@users.noreply.github.com>
2020-06-23 19:47:19 -0300

    Create Funcoes.py

---
+++ Funcoes.py
@@ -0,0 +1,20 @@
+def sayhi():
+    print('Hello User')
+
+sayhi()
+
+def says(name):
+    print(f'Hello user {name}')
+
+says('Vitor')
+says('Michael')
+
+def cube(num):
+    return num * num * num
+
+print(cube(5))
+
+def max_num (num1, num2, num3):
+    print(max(num1,num2,num3))
+
+max_num(50,60,70)
