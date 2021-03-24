8fdcb69129c2a018868ff501123caeff49a25a43
Vitor <64867044+Vvitoor21@users.noreply.github.com>
2021-03-24 20:34:20 -0300

    Create 6_Criando_Boletim_Pandas.py

---
+++ 6_Criando_Boletim_Pandas.py
@@ -0,0 +1,19 @@
+import pandas as pd
+boletim = pd.DataFrame({'notas1': [8, 5, 9, 8, 7],
+            'notas2': [8, 4, 6, 4, 5],
+            'materia': ['bio', 'mat', 'por', 'qui', 'fis']
+                        })
+
+print(boletim)
+
+boletim['media'] =  ( boletim['notas1'] + boletim['notas2'] ) // 2
+
+print(boletim)
+
+for index, row in boletim.iterrows():
+   if row['media'] <= 6 :
+       boletim.loc[index, 'situacao'] = 'REPROVADO'
+   else:
+       boletim.loc[index, 'situacao'] = 'APROVADO'
+
+print(boletim)
