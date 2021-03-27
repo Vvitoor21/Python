f5206fbe528405b2361ab37c2653695053de0efe
Vitor <64867044+Vvitoor21@users.noreply.github.com>
2021-03-27 13:49:17 -0300

    Delete Senha.py

--- Senha.py
+++
@@ -1,9 +0,0 @@
-senha = 'vitorduarte'
-acesso=''
-print('Para acessar digite sua senha:')
-
-while acesso != senha:
-    acesso=input()
-    print('senha errada')
-    if acesso == senha:
-        print('Bem vindo')
