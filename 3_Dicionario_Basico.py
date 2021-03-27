9a304a67cda8b9b5ee59bb0d4ad1540fe8778818
Vitor <64867044+Vvitoor21@users.noreply.github.com>
2021-03-27 13:51:48 -0300

    Update 3_Dicionario_Basico.py

--- 3_Dicionario_Basico.py
+++ 3_Dicionario_Basico.py
@@ -1,15 +1,21 @@
-months={'Jan':'January','Feb':'February','Mar':'March','Apr':'April',}
+months={'Jan':'January','Feb':'February','Mar':'March','Apr':'April'}
 print('O dicionario completo é:')
 print(months)
+
 print('Os valores do dicionario são:')
 print(months.values())
+
 print('As chaves dos dicionarios são:')
 print(months.keys())
+
 months['Ma']='May'
 months['Ju']='June'
 months['Jul']='July'
+
 print(months)
+
 months['JAN'] =months['Jan']
 del months['Jan']
+
 print(months)
 print(months.items())
