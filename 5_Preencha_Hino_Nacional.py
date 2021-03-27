27afc004d0ea4d562741d53332acd8a8aad56c6a
Vitor <vitor.duartegot@gmail.com>
2021-03-27 13:41:44 -0300

    Update 5_Preencha_Hino_Nacional.py

--- 5_Preencha_Hino_Nacional.py
+++ 5_Preencha_Hino_Nacional.py
@@ -1,11 +1,18 @@
 print('De acordo com o site da Terra:\n58,4% dos brasileiros não sabem cantar o hino nacional')
 
-print('Você consegue acertar as palavras com esse teste ? ')
+print('Você consegue acertar as palavras com esse teste ? \n')
+
+#Atividade para treinamento do Loop WHILE
 
 nome1= 'plácidas'
 nome2='retumbante'
 nome3='fúlgidos'
 nome4='instante'
+
+nome5= 'penhor'
+nome6 = 'braço'
+nome7 = 'liberdade'
+nome8 = 'desafia'
 
 musica=''
 
@@ -29,7 +36,38 @@ while musica != nome4:
     if musica != nome4:
         print('errado')
 
-print(f'0uviram do ipiranga as margens {nome1}.')
-print(f'De um povo heroico o brado {nome2}.')
-print(f'E o sol da liberdade em raios {nome3}.')
-print(f'Brilhou no céu da pátria nesse {nome4}.')
+while musica != nome5:
+    musica = input('Se o "?" dessa igualdade: ')
+    if musica != nome5:
+        print('errado')
+
+while musica != nome6:
+    musica = input('Conseguimos conquistar com " ? " forte, : ')
+    if musica != nome6:
+        print('errado')
+
+while musica != nome7:
+    musica = input('Em teu seio, ó : ')
+    if musica != nome7:
+        print('errado')
+
+while musica != nome8:
+    musica = input('" ? " o nosso peito a própria morte! : ')
+    if musica != nome8:
+        print('errado')
+
+print('Letra com as respostas corretas : \n')
+
+print(f'\n0uviram do ipiranga as margens {nome1}.')
+print(f'De um povo heroico o brado {nome2},')
+print(f'E o sol da liberdade em raios {nome3},')
+print(f'Brilhou no céu da pátria nesse {nome4}.\n')
+
+print(f'Se o {nome5} dessa igualdade')
+print(f'Conseguimos conquistar com {nome6} forte,')
+print(f'Em teu seio, ó {nome7},')
+print(f'{nome8} o nosso peito a própria morte!\n')
+
+print('Ó Patria amada,')
+print('Idolatrada,')
+print('Salve! Salve!\n')
