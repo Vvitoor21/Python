ff53044a47b5e77cb830930ee2ddc0b632c7449c
Vitor <64867044+Vvitoor21@users.noreply.github.com>
2021-03-30 15:09:59 -0300

    Update 9_Participantes_Reality.py

--- 9_Participantes_Reality.py
+++ 9_Participantes_Reality.py
@@ -13,6 +13,8 @@ idade = int(input())
 print('Informe sua profissao')
 prof = input()
 
+# --------------------------------------------------------------------------------------------------------------
+
 class Usuario:
     def __init__(self):
         self.nome = nome
@@ -73,10 +75,24 @@ class Pessoa4:
     def __repr__(self):
         return str(self.__dict__)
 
+class Pessoa5:
+    def __init__(self):
+        self.nome = 'Priscila'
+        self.idade = 37
+        self.profissao = 'Veterinaria'
+
+    def mostra_informacoes(self):
+        print(self.nome, str(self.idade)+' anos',' profissao: '+ self.profissao)
+
+    def __repr__(self):
+        return str(self.__dict__)
+
+# ---------------------------------------------------------------------------------------------------------
+
 usuario = Usuario()
 
 time.sleep(2)
-print(f'Muito prazer {usuario.nome}. Você tem {usuario.idade} anos  e trabalha como {usuario.profissao}. Muito bom!!! Aqui abaixo esta as informacoes dos outros 4 participantes')
+print(f'Muito prazer {usuario.nome}. Você tem {usuario.idade} anos  e trabalha como {usuario.profissao}. Muito bom!!! Aqui abaixo estão as informações dos outros participantes')
 
 print('----------------------------------------------------------------------------------')
 
@@ -84,14 +100,15 @@ felipe = Pessoa1()
 amanda = Pessoa2()
 arthur = Pessoa3()
 paula = Pessoa4()
+priscila = Pessoa5()
 
 time.sleep(10)
 print('LISTA DE PARTICIPANTES : ')
 
-lista = [ felipe.nome, amanda.nome, arthur.nome, paula.nome, usuario.nome]
+lista = [ felipe.nome, amanda.nome, arthur.nome, paula.nome, priscila.nome, usuario.nome]
 print(lista)
 time.sleep(2)
-print('PRINCIPAIS INFORMACOES')
+print('PRINCIPAIS INFORMAÇÕES ')
 time.sleep(3)
 felipe.mostra_informacoes()
 time.sleep(2)
@@ -101,16 +118,18 @@ arthur.mostra_informacoes()
 time.sleep(2)
 paula.mostra_informacoes()
 time.sleep(2)
+priscila.mostra_informacoes()
+time.sleep(2)
 usuario.mostra_informacoes()
 
-time.sleep(2)
-print('CONTAGEM REGRESSIVA: ')
+time.sleep(4)
+print('CONTAGEM REGRESSIVA PARA INÍCIO DO PROGRAMA : ')
 
 for n in range(10, -1, -1):
     time.sleep(1)
     print(n)
 
-frase = "BIG BROTHER BRASIL"
+frase = "* * * BIG BROTHER BRASIL * * *"
 
 import sys
 
@@ -130,13 +149,13 @@ for i in ' - - - 1° PERGUNTA - - - :':
 
 import random
 
-print('\nQuantos animais de cada espécie, Moisés colocou em sua arca: 1, 2 ou 3? DIGITE O NUMERO ')
+print('\nQuantos animais de cada espécie, Moisés colocou em sua arca: 1, 2 ou 3? DIGITE O NUMERO: ')
 
 resp = int(input())
 
 if resp == 0:
     time.sleep(3)
-    print('VOCCÊ É O LIDER DA RODADA !!!!\n')
+    print('VOCÊ É O LIDER DA RODADA !!!!\n')
     lista.remove(usuario.nome)
     eliminado = random.choice(lista)
     time.sleep(3)
@@ -162,18 +181,20 @@ else:
     lista.remove(random.choice(lista))
     lista.append(usuario.nome)
     time.sleep(2)
+    print(f'O E liminado foi : {escolhido}')
     print(f'\nVoltaram :{lista}')
     lista.append(escolhido)
 
 time.sleep(3)
 print(f'SEGUEM NO JOGO: {lista}')
 
-for i in ' - - - 2° PERGUNTA - - - :':
+for i in ' - - - 2° PERGUNTA - - - : \n':
     print(i, end=' ')
     time.sleep(0.40)
 
 time.sleep(2)
 print('ATENÇÃO\n')
+
 time.sleep(2)
 print('No dia 7 de setembro, nós comemoramos o Dia da Independência no Brasil.'
       'Em Portugal, também existe 7 de setembro ??? \n')
@@ -185,7 +206,7 @@ dia = int(input())
 if dia == 1:
     print('Você é esperto. E agora é o líder !!!')
     time.sleep(3)
-    print('Vamos deixar os proximos 3 participantes enfrentarem o paredão...')
+    print('Vamos deixar os proximos  participantes enfrentarem o paredão...')
     lista.remove(usuario.nome)
     eliminado2 = random.choice(lista)
     time.sleep(3)
@@ -196,7 +217,7 @@ if dia == 1:
     print(f'O ELIMANDO FOI: {eliminado2}')
 else:
     for i in ' Claro que sim, bem como nos Estados Unidos, na Espanha, na Austrália... '\
-             'Basta olhar o calendário desses países e reparar que 7 de setembro estará lá, assim como todos os outros dias do ano':
+             'Basta olhar o calendário desses países e reparar que 7 de setembro estará lá, assim como todos os outros dias do ano\n':
         print(i, end='')
         time.sleep(0.10)
 
@@ -218,10 +239,54 @@ else:
     lista.append(escolhido2)
     print('TEM SORTE DE AINDA NÃO TER SIDO ELIMINADO')
 
-
 time.sleep(3)
 print(f'SEGUEM NO JOGO:{lista}')
 
+for n in ' - - - 3° E ÚLTIMA PERGUNTA - - - \n':
+    print(n, end=' ')
+    time.sleep(0.10)
+
+time.sleep(2)
+print('\nA mãe de Mário tem três filhos. O primeiro se chama Março e o segundo se chama Abril. Qual é o nome do terceiro ?')
+print('Digite 1 - Junho, 2 - Mário, 3 - Maio , 4 - Fevereiro')
+
+nome = int(input())
+
+while nome not in (1, 2, 3, 4):
+    print('Não entendi..Digite Novamente')
+    nome = int(input())
+if nome == 2:
+    print('ACERTOU !!!')
+    time.sleep(3)
+    print('Vamos deixar os proximos 3 participantes enfrentarem o paredão...')
+    lista.remove(usuario.nome)
+    eliminado3 = random.choice(lista)
+    time.sleep(3)
+    print(f'O PAREDÃO ESTÁ ENTRE: {lista}\n')
+    lista.remove(eliminado3)
+    lista.append(usuario.nome)
+    time.sleep(3)
+    print(f'O ELIMANDO FOI: {eliminado3}')
+    if resp in (1, 3, 4):
+        for n in 'A prória pergunta já diz a resposta...." A mãe de Mário tem três filhos. O primeiro se chama Março e o segundo se chama Abril" ':
+            print(n, end=' ')
+            time.sleep(0.10)
+        time.sleep(3)
+        print('OBVIAMENTE O 3° É MARIO. VOCÊ ERROU !!!! ')
+        lista.remove(usuario.nome)
+        escolhido3 = random.choice(lista)
+        time.sleep(2)
+        print(f'\nO LIDER É: {escolhido3}')
+        lista.remove(escolhido3)
+        lista.append(usuario.nome)
+        time.sleep(2)
+        print(f'\nESTÃO NO PAREDÃO: {lista}')
+        lista.remove(usuario.nome)
+        lista.remove(random.choice(lista))
+        lista.append(usuario.nome)
+        time.sleep(2)
+        print(f'\nVoltaram :{lista}\n')
+        lista.append(escolhido3)
 
 print(f'\nJOGAM A FINAL: {lista}')
 time.sleep(5)
@@ -236,9 +301,11 @@ for n in range(10,0,-1):
     time.sleep(1)
     print(n)
 
-for l in 'O VENCEDOR É : : : : : : : : :':
+lista.remove(usuario.nome)
+for l in ' O VENCEDOR É : : : : : : : : ':
     print(l, end=' ')
     time.sleep(0.25)
 vencedor = random.choice(lista)
 
+time.sleep(4)
 print(f'\n{vencedor} !!!!! PARABÉNS. 1,5 MILHÕES DE REAIS !!!')
