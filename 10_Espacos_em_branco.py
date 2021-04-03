b9cd22d523cfa18f96bf2fe5752cbf8c636d9650
Vitor <64867044+Vvitoor21@users.noreply.github.com>
2021-04-03 20:24:27 -0300

    Create 10_Espacos_em_branco.py

---
+++ 10_Espacos_em_branco.py
@@ -0,0 +1,11 @@
+nome = '     Vitor  '
+nome = nome.strip()
+print(nome)
+
+nome2 = 'Bianca      '
+nome2 = nome2.rstrip()
+print(nome2)
+
+nome3 = '     Hiago'
+nome3 = nome3.lstrip()
+print(nome3)
