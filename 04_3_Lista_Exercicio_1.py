9c66b0e1008c3fb742b1f64285a7e2810dd2eda7
Vitor <64867044+Vvitoor21@users.noreply.github.com>
2021-05-01 15:02:38 -0300

    Create 04_3_Lista_Exercicio_1.py

---
+++ 04_3_Lista_Exercicio_1.py
@@ -0,0 +1,14 @@
+pessoas_festa = ['Rogerio', 'Paula', 'Carolina', 'Anderson', 'Felipe', 'Marcos', 'Bruna']
+pessoas_festa.sort()# Uma ordem crescente
+print(pessoas_festa)
+pessoas_festa.sort(reverse=True)# Uma ordem decrescente
+print(pessoas_festa)
+pessoas_festa.sort()
+print(pessoas_festa)
+numeros = [1, 5, 4, 7, 8, 2]
+print(f'Numeros ordenados temporariamente:{sorted(numeros)}')
+print(f'Numeros desordenados:{numeros}')
+numeros.reverse()
+print(f'Numeros revertidos: {numeros}')
+
+lugares_mundo = ['Rio','França','Nova York','Amazonia','Madri']
