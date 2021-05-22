c70dd643a031486ac42159dd17a6b0346da444dc
Vitor <vitor.duartegot@gmail.com>
2021-05-22 16:44:35 -0300

    Update 04_4_Lista_Exercicio.py

--- 04_4_Lista_Exercicio.py
+++ 04_4_Lista_Exercicio.py
@@ -1,1 +1,16 @@
+pizzas = ['Camarão', 'Mussarela', 'Portuguesa']
+for pizza in pizzas:
+    print(pizza.title())
 
+print(f'\n')
+
+for indice, pizza in enumerate(pizzas):
+    print(f'{indice+1}° pizza desejada: {pizza}')
+
+if len(pizzas) >=3:
+    print('\nEu realmente gosto de comer pizza\n')
+
+animais = ['cachorro','pato','cobra']
+
+for animal in animais:
+    print(f'{animal.title()} é um dos meus animais de estimação')
